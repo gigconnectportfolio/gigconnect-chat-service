ef49c43f79863434e12495644180aea2f7fedb8e
Keith <karirukeith@gmail.com>
2025-10-27 15:50:27 +0300

    implemented conversation and message models

---
+++ src/models/conversation.schema.ts
@@ -0,0 +1,11 @@
+import {model, Model, Schema} from "mongoose";
+import {IConversationDocument} from "@kariru-k/gigconnect-shared";
+
+const conversationSchema: Schema = new Schema({
+    conversationId: {type: String, required: true, unique: true, index: true},
+    senderUsername: {type: String, required: true, index: true},
+    receiverUsername: {type: String, required: true, index: true},
+});
+
+const ConversationModel: Model<IConversationDocument> = model<IConversationDocument>('Conversation', conversationSchema, 'Conversation');
+export {ConversationModel};
